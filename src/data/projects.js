df31725115cb95906694c2e4e1e6d36c3f9675f0
DiaaEldinHassan <diaahassan777@gmail.com>
2026-05-12 00:30:06 +0300

    Added New Projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,22 @@ import ecommerce from "../assets/ecommerce.png";
 
 export const projects = [
   {
-    title: "🌿 Smart Irrigation System – Flutter + Arduino + Firebase",
+    title: "🍽️ Food Order - Full Stack Online Food Ordering App 🍽️",
+    description: "A full-stack online food ordering platform built with **React 19**, **Express 5**, and **MongoDB**. Users can browse restaurants, order food, track deliveries, and manage their profile — with admin controls for restaurants and menu items.",
+    image: "https://iotbusinessnews.com/WordPress/wp-content/uploads/2024/07/smart-irrigation-city.jpg",
+    technologies: ["nodejs", "express", "react", "mongodb", "tailwindcss", "s3", "aws", "jwt", "zod"],
+    link: "https://github.com/DiaaEldinHassan/Online-Food-Order-App.git",
+    fallbackColor: "from-orange-400 to-white-500"
+  },
+  {
+    title: "💬 Sarahah Anonymous Messaging App – A Full Stack Web App",
+    description: "A full-stack anonymous messaging application where users can receive honest, anonymous messages from anyone. Built with React on the frontend and Node.js/Express on the backend.",
+    image: "https://iotbusinessnews.com/WordPress/wp-content/uploads/2024/07/smart-irrigation-city.jpg",
+    technologies: ["nodejs", "express", "react", "mongodb", "tailwindcss", "2fa"],
+    link: "https://github.com/DiaaEldinHassan/SIS",
+    fallbackColor: "from-green-400 to-blue-500"
+  },{
+    title: "🌿 Smart Irrigation System – Flutter + Arduino + Firebase - Graduation Project",
     description: "This project is a smart irrigation system built with Flutter for the mobile app frontend, Arduino for hardware control, and Firebase as the real-time backend. The system allows users to remotely monitor and control irrigation through an intuitive mobile interface.",
     image: "https://iotbusinessnews.com/WordPress/wp-content/uploads/2024/07/smart-irrigation-city.jpg",
     technologies: ["Flutter", "Dart", "Firebase", "Arduino", "IoT"],
@@ -11,23 +26,7 @@ export const projects = [
     fallbackColor: "from-green-400 to-blue-500"
   },
   {
-    title: "📝 Notes – Simple, Stylish, and Functional",
-    description: "A full-stack notes/blog management application with user authentication, built with React and Node.js/Express.",
-    image: "https://github.com/DiaaEldinHassan/Notes-App/blob/main/screenshots/dashboard.png",
-    technologies: ["React", "Tailwind", "NodeJS", "Express", "JWT", "Google Authentication"],
-    link: "https://github.com/DiaaEldinHassan/Notes-App.git",
-    fallbackColor: "from-yellow-400 to-orange-500"
-  },
-  {
-    title: "🌦️ Weather App – Real-Time Weather with JavaScript & EJS",
-    description: "A responsive and dynamic weather application built using JavaScript, EJS (Embedded JavaScript Templates), and CSS, powered by a public Weather API.",
-    image: "https://images.pexels.com/photos/1118873/pexels-photo-1118873.jpeg",
-    technologies: ["Javascript", "EJS", "API", "NodeJs", "Express", "CSS"],
-    link: "https://github.com/DiaaEldinHassan/Weather",
-    fallbackColor: "from-blue-400 to-cyan-500"
-  },
-  {
-    title: "🛒 E-Commerce Web Application",
+    title: "🛒 E-Commerce Web Application - Frontend Web App",
     description: "A modern, fully responsive e-commerce web application built with React, React Router, and Tailwind CSS.",
     image: ecommerce,
     technologies: ["HTML", "CSS", "JavaScript", "React", "Tailwind"],
