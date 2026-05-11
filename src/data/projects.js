60897d060986636dbbe27d8f9319798316672db0
DiaaEldinHassan <diaahassan777@gmail.com>
2026-05-12 00:37:29 +0300

    Added New Projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,36 +1,61 @@
-
 import ecommerce from "../assets/ecommerce.png";
 
 export const projects = [
   {
     title: "🍽️ Food Order - Full Stack Online Food Ordering App 🍽️",
-    description: "A full-stack online food ordering platform built with **React 19**, **Express 5**, and **MongoDB**. Users can browse restaurants, order food, track deliveries, and manage their profile — with admin controls for restaurants and menu items.",
-    image: "https://github.com/DiaaEldinHassan/Online-Food-Order-App/blob/main/Screenshots/mainPage.png",
-    technologies: ["nodejs", "express", "react", "mongodb", "tailwindcss", "s3", "aws", "jwt", "zod"],
+    description:
+      "A full-stack online food ordering platform built with **React 19**, **Express 5**, and **MongoDB**. Users can browse restaurants, order food, track deliveries, and manage their profile — with admin controls for restaurants and menu items.",
+    image:
+      "https://raw.githubusercontent.com/DiaaEldinHassan/Online-Food-Order-App/refs/heads/main/Screenshots/mainPage.png",
+    technologies: [
+      "nodejs",
+      "express",
+      "react",
+      "mongodb",
+      "tailwindcss",
+      "s3",
+      "aws",
+      "jwt",
+      "zod",
+    ],
     link: "https://github.com/DiaaEldinHassan/Online-Food-Order-App.git",
-    fallbackColor: "from-orange-400 to-white-500"
+    fallbackColor: "from-orange-400 to-white-500",
   },
   {
     title: "💬 Sarahah Anonymous Messaging App – A Full Stack Web App",
-    description: "A full-stack anonymous messaging application where users can receive honest, anonymous messages from anyone. Built with React on the frontend and Node.js/Express on the backend.",
-    image: "https://github.com/DiaaEldinHassan/Anonymous-Messages/blob/main/Screenshots/Landing%20Page.png",
-    technologies: ["nodejs", "express", "react", "mongodb", "tailwindcss", "2fa"],
+    description:
+      "A full-stack anonymous messaging application where users can receive honest, anonymous messages from anyone. Built with React on the frontend and Node.js/Express on the backend.",
+    image:
+      "https://raw.githubusercontent.com/DiaaEldinHassan/Anonymous-Messages/refs/heads/main/Screenshots/Landing%20Page.png",
+    technologies: [
+      "nodejs",
+      "express",
+      "react",
+      "mongodb",
+      "tailwindcss",
+      "2fa",
+    ],
     link: "https://github.com/DiaaEldinHassan/SIS",
-    fallbackColor: "from-green-400 to-blue-500"
-  },{
-    title: "🌿 Smart Irrigation System – Flutter + Arduino + Firebase - Graduation Project",
-    description: "This project is a smart irrigation system built with Flutter for the mobile app frontend, Arduino for hardware control, and Firebase as the real-time backend. The system allows users to remotely monitor and control irrigation through an intuitive mobile interface.",
-    image: "https://iotbusinessnews.com/WordPress/wp-content/uploads/2024/07/smart-irrigation-city.jpg",
+    fallbackColor: "from-green-400 to-blue-500",
+  },
+  {
+    title:
+      "🌿 Smart Irrigation System – Flutter + Arduino + Firebase - Graduation Project",
+    description:
+      "This project is a smart irrigation system built with Flutter for the mobile app frontend, Arduino for hardware control, and Firebase as the real-time backend. The system allows users to remotely monitor and control irrigation through an intuitive mobile interface.",
+    image:
+      "https://iotbusinessnews.com/WordPress/wp-content/uploads/2024/07/smart-irrigation-city.jpg",
     technologies: ["Flutter", "Dart", "Firebase", "Arduino", "IoT"],
     link: "https://github.com/DiaaEldinHassan/SIS",
-    fallbackColor: "from-green-400 to-blue-500"
+    fallbackColor: "from-green-400 to-blue-500",
   },
   {
     title: "🛒 E-Commerce Web Application - Frontend Web App",
-    description: "A modern, fully responsive e-commerce web application built with React, React Router, and Tailwind CSS.",
+    description:
+      "A modern, fully responsive e-commerce web application built with React, React Router, and Tailwind CSS.",
     image: ecommerce,
     technologies: ["HTML", "CSS", "JavaScript", "React", "Tailwind"],
     link: "https://e-commerce-gules-psi.vercel.app/",
-    fallbackColor: "from-purple-400 to-pink-500"
-  }
+    fallbackColor: "from-purple-400 to-pink-500",
+  },
 ];
